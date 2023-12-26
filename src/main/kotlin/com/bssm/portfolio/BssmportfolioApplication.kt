a8cce5a984fdd0cd856e4cc1f7298a2975f2f9a5
anys34 <anyeseong34@gmail.com>
2023-12-26 10:33:13 +0900

    ADD :: proj setting

---
+++ src/main/kotlin/com/bssm/portfolio/BssmportfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.bssm.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BssmportfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<BssmportfolioApplication>(*args)
+}
