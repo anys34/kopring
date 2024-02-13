9e6ca07eb778eb0e3133b02af907369039719b89
anys34 <anyeseong34@gmail.com>
2024-02-13 22:25:10 +0900

    사칙연산 계산기 테스트 코드 작성

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,7 +1,7 @@
 package com.group.libraryapp.calculator
 
 class Calculator(
-    private var number: Int
+    var number: Int
 ) {
     fun add(operand: Int) {
         this.number += operand
--- src/test/kotlin/com/group/librayapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/librayapp/calculator/CalculatorTest.kt
@@ -5,15 +5,46 @@ import com.group.libraryapp.calculator.Calculator
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
 }
 
 class CalculatorTest {
     fun addTest() {
+        // given
         val calculator = Calculator(5)
+
+        // when
         calculator.add(3)
 
-        val expectedCalculator = Calculator(8)
-        if (calculator != expectedCalculator) {
+        // then
+        if (calculator.number != 8) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
             throw IllegalArgumentException()
         }
     }
